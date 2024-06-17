8a2408bed9d2234704b0187def6333409e37ff0b
afanasevkirill <67817268+afanasevkirill@users.noreply.github.com>
2024-06-17 20:54:57 +0300

    add questions as in Kashima, Y., Wilson, S., Lusher, D., Pearson, L. J., & Pearson, C. (2013). The acquisition of perceived descriptive norms as social category learning in social networks. Social Networks, 35(4), 711–719. https://doi.org/10.1016/j.socnet.2013.06.002 + normative expectation and personal normative beliefs elicitation a-la Bicchieri, C., & Chavez, A. (2010). Behaving as expected: Public information and fairness norms. https://onlinelibrary.wiley.com/doi/abs/10.1002/bdm.648

--- coordinatenetwork/coordination_game/__init__.py
+++ coordinatenetwork/coordination_game/__init__.py
@@ -3,23 +3,29 @@ import random
 from statistics import multimode
 
 def eval_options(code):
-    mapping = {-2:"Социально неприемлемо",
-               -1: "Скорее социально неприемлемо",
-                1: "Скорее социально приемлемо",
-                2: "Социально приемлемо",
+    mapping = {1:"абсолютно не согласен",
+               2: "скорее не согласен",
+                3: "затрудняюсь ответить",
+                4: "скорее согласен",
+               5: "абсолютно согласен",
                0: "Отказ от ответа"
                }
     return mapping[code]
 
+decisions = {'engagement_1': 'Я всегда стараюсь защитить этот город и его окружающую среду от любого вреда.',
+            'engagement_2': 'Я всегда энергично работаю над улучшением этого города и его окружающей среды, насколько это возможно.',
+             'ee_1': 'Жители этого города вкладывают много энергии в совместную работу — как городское сообщество — над улучшением города и его окружающей среды.',
+             'ee_2': 'Жители этого города очень стараются работать вместе — как городское сообщество — для защиты города и его окружающей среды.',
+             'ident_1': 'Я чувствую сильную связь с этим городом или привязанность к нему.',
+             'ident_2': 'Я твердо чувствую себя обязанным городу.',
+             'ident_3': 'Я продолжу жить в городе в обозримом будущем.',
+             'nature': 'Я чувствую сильную связь или привязанность к окружающей среде.',
+             'pnb_1': 'Я считаю, что жители города должны вкладывать много энергии в совместную — как сообщество — над улучшением города и его окружающей среды.',
+             'pnb_2': 'Я считаю, что жители города должны очень стараться работать вместе — как сообщество — для защиты города и его окружающей среды.',
+             'ne_1': '''Предыдущие участники, так же как и вы, отмечали насколько они согласны со следующим утверждением: "Я считаю, что жители города должны вкладывать много энергии в совместную — как сообщество — над улучшением города и его окружающей среды". Выберите вариант ответа, который, как вам кажется, предыдущие участники эксперимента выбирали чаще всего.''',
+                'ne_2': '''Предыдущие участники, так же как и вы, отмечали насколько они согласны со следующим утверждением: "Я считаю, что жители города должны очень стараться работать вместе — как сообщество — для защиты города и его окружающей среды." Выберите вариант ответа, который, как вам кажется, предыдущие участники эксперимента выбирали чаще всего. ''',
 
-decisions = {'eval_not_demand': 'Участник Б ничего не запросил у участника А. Оцените поведение участника Б.',
-          'eval_demand': 'Участник Б запросил у участника А 15 очков из положенных 25 очков. Оцените поведение участника Б.',
-          'eval_pay': 'Участник А согласился отдавать участнику Б 15 запрошенных очков. Оцените поведение участника А.',
-          'eval_not_pay': 'Участник А отказался отдавать участнику Б 15 запрошенных очков. Оцените поведение участника А.',
-          'eval_punish_B': 'Участник Б запросил часть вознаграждения у участника А. Наблюдая это, участник В уменьшает выигрыш участника Б на 5 очков. Оцените поведение участника В.',
-          'eval_not_punish_B': 'Участник Б запросил часть вознаграждения у участника А. Наблюдая это, участник В не уменьшает выигрыш участника Б. Оцените поведение участника В.',
-          'eval_punish_A': 'Участник А соглашается отдать часть приза участнику Б. Наблюдая это, участник В уменьшает выигрыш участника А на 5 очков. Оцените поведение участника В.',
-          'eval_not_punish_A': 'Участник А соглашается отдать часть приза участнику Б. Наблюдая это, участник В не уменьшает выигрыш участника А на 5 очков. Оцените поведение участника В.',
+
                                                           }
 
 
@@ -27,7 +33,7 @@ class C(BaseConstants):
     NAME_IN_URL = 'coordination_game'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
-    PRIZE = cu(200)
+    PRIZE = cu(250)
 
 
 class Subsession(BaseSubsession):
@@ -42,49 +48,60 @@ class Player(BasePlayer):
     mode_answer = models.StringField()
 
     def set_payoff(self):
-        self.random_decision = random.choice(['eval_not_demand', 'eval_demand', 'eval_pay', 'eval_not_pay'])
+        self.random_decision = random.choice(['ne_1', 'ne_2'])
         list_of_decisions = eval(f'[p.field_maybe_none("{self.random_decision}") for p in self.subsession.get_players()]')
         mode_answers_list = multimode([x for x in list_of_decisions if x is not None])
         self.mode_answer = ', '.join([eval_options(item) for item in mode_answers_list])
         if eval(f'self.{self.random_decision}') in mode_answers_list:
             self.payoff = C.PRIZE
         else:
-            self.payoff = cu(100)
+            self.payoff = cu(50)
 
     def make_field(label):
         return models.IntegerField(
             choices=[
-                [-2, 'Социально неприемлемо'],
-                [-1, 'Скорее социально неприемлемо'],
-                [1, 'Скорее социально приемлемо'],
-                [2, 'Социально приемлемо'],
+                [1, 'абсолютно не согласен'],
+                [2, 'скорее не согласен'],
+                [3, 'затрудняюсь ответить'],
+                [4, 'скорее согласен'],
+                [5, 'абсолютно согласен'],
             ],
+            widget=widgets.RadioSelect,
             label=label
         )
 
+    city = models.StringField(
+        label='В каком городе вы в данный момент постоянно проживаете?')
+    engagement_1 = make_field(decisions['engagement_1'])
+    engagement_2 = make_field(decisions['engagement_2'])
+    ee_1 = make_field(decisions['ee_1'])
+    ee_2 = make_field(decisions['ee_2'])
+    ident_1 = make_field(decisions['ident_1'])
+    ident_2 = make_field(decisions['ident_2'])
+    ident_3 = make_field(decisions['ident_3'])
+    nature = make_field(decisions['nature'])
+    pnb_1 = make_field(decisions['pnb_1'])
+    pnb_2 = make_field(decisions['pnb_2'])
+    ne_1 = make_field(decisions['ne_1'])
+    ne_2 = make_field(decisions['ne_2'])
 
-    eval_not_demand = make_field(decisions['eval_not_demand'])
-    eval_demand = make_field(decisions['eval_demand'])
-    eval_pay = make_field(decisions['eval_pay'])
-    eval_not_pay = make_field(decisions['eval_not_pay'])
 
-    eval_punish_B = make_field(decisions['eval_punish_B'])
-    eval_not_punish_B = make_field(decisions['eval_not_punish_B'])
-    eval_punish_A = make_field(decisions['eval_punish_A'])
-    eval_not_punish_A = make_field(decisions['eval_not_punish_A'])
 
 # FUNCTIONS
 # PAGES
 
-
+class Introduction(Page):
+    form_model = 'player'
+    form_fields = []
 
 class Rating1(Page):
     form_model = "player"
-    form_fields = ['eval_not_demand','eval_demand', 'eval_pay', 'eval_not_pay']
+    form_fields = ['engagement_1','engagement_2', 'ee_1', 'ee_2', 'ident_1', 'ident_2', 'ident_3', 'nature', 'pnb_1', 'pnb_2']
 
 class Rating2(Page):
     form_model = "player"
-    form_fields = ['eval_punish_B', 'eval_not_punish_B', 'eval_punish_A', 'eval_not_punish_A']
+    timeout_seconds = 120
+    form_fields = ['ne_1', 'ne_2']
 
     def before_next_page(player: Player, timeout_happened):
         player.set_payoff()
@@ -104,7 +121,7 @@ class Results(Page):
         )
 
 
-page_sequence = [
+page_sequence = [Introduction,
                  Rating1,
                  Rating2,
                 Results
--- coordinatenetwork/settings.py
+++ coordinatenetwork/settings.py
@@ -5,7 +5,7 @@ SESSION_CONFIGS = [
     dict(
         name='net_experiment',
         display_name="Network experiment",
-        app_sequence=['survey', 'coordination_game', 'final'],
+        app_sequence=['coordination_game', 'survey', 'final'],
         num_demo_participants=1,
     ),
 ]
